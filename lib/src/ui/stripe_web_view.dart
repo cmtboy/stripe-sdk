49bb282fbdf10d3e4d4c7a36abd3a48a84bca526
AL Amin spn103 <spn103@spondonit.com>
2024-08-08 17:47:28 +0600

    update webview_flutter to v4.8.0

--- lib/src/ui/stripe_web_view.dart
+++ lib/src/ui/stripe_web_view.dart
@@ -1,26 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
-class StripeWebView extends StatelessWidget {
-  const StripeWebView({Key? key, required this.uri, required this.returnUri}) : super(key: key);
+class StripeWebView extends StatefulWidget {
+  const StripeWebView({Key? key, required this.uri, required this.returnUri})
+      : super(key: key);
 
   final String uri;
   final Uri returnUri;
 
   @override
+  _StripeWebViewState createState() => _StripeWebViewState();
+}
+
+class _StripeWebViewState extends State<StripeWebView> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the WebViewController
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    _controller = WebViewController.fromPlatformCreationParams(params);
+
+    _controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (NavigationRequest request) {
+            final uri = Uri.parse(request.url);
+            if (uri.scheme == widget.returnUri.scheme &&
+                uri.host == widget.returnUri.host &&
+                uri.queryParameters['requestId'] ==
+                    widget.returnUri.queryParameters['requestId']) {
+              Navigator.pop(context, true);
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.uri));
+
+    // Enable debugging and other platform-specific features
+    if (_controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (_controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return WebView(
-        initialUrl: uri,
-        javascriptMode: JavascriptMode.unrestricted,
-        navigationDelegate: (navigation) {
-          final uri = Uri.parse(navigation.url);
-          if (uri.scheme == returnUri.scheme &&
-              uri.host == returnUri.host &&
-              uri.queryParameters['requestId'] == returnUri.queryParameters['requestId']) {
-            Navigator.pop(context, true);
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        });
+    return WebViewWidget(controller: _controller);
   }
 }
